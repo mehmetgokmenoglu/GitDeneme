67c4ca119cfd19fa72d9cfa9055b0ab611a582d4
Mehmet Gokmenoglu <gokmenoglumehmet3@gmail.com>
2024-12-04 20:33:35 +0300

    enum created

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
+import Model.TransportMethod;
+import Model.Vehicle;
+
 public class Main {
     public static void main(String[] args) {
+        Vehicle truck = new Vehicle("Truck", TransportMethod.ROAD, 10.0, 80.0);
+        Vehicle airplane = new Vehicle("Airplane", TransportMethod.AIR, 5.0, 600.0);
+        Vehicle ship = new Vehicle("Ship", TransportMethod.SEA, 50.0, 40.0);
+
+        System.out.println(truck.getName() + " Speed: " + truck.calculateSpeed() + " km/h");
+        System.out.println(truck.getName() + " Cost Per Km: " + truck.calculateCostPerKm() + " TL/km");
+
+        System.out.println(airplane.getName() + " Speed: " + airplane.calculateSpeed() + " km/h");
+        System.out.println(airplane.getName() + " Cost Per Km: " + airplane.calculateCostPerKm() + " TL/km");
+
+        System.out.println(ship.getName() + " Speed: " + ship.calculateSpeed() + " km/h");
+        System.out.println(ship.getName() + " Cost Per Km: " + ship.calculateCostPerKm() + " TL/km");
     }
 }
--- src/Model/Vehicle.java
+++ src/Model/Vehicle.java
@@ -1,4 +1,71 @@
 package Model;
 
-public class Vehicle {
+public class Vehicle implements Transportable {
+
+    private String name;
+    private TransportMethod transportMethod;
+    private double capacity;
+    private double baseSpeed;
+
+    public Vehicle(String name, TransportMethod transportMethod, double capacity, double baseSpeed) {
+        this.name = name;
+        this.transportMethod = transportMethod;
+        this.capacity = capacity;
+        this.baseSpeed = baseSpeed;
+    }
+
+    @Override
+    public double calculateSpeed() {
+        switch (transportMethod) {
+            case ROAD: return baseSpeed; // Kara için hız
+            case AIR: return baseSpeed * 2.0;  // Hava için hız
+            case SEA: return baseSpeed * 0.5;  // Deniz için hız
+            default: throw new IllegalArgumentException("Bilinmeyen taşıma yöntemi!");
+        }
+    }
+
+    @Override
+    public double calculateCostPerKm() {
+        switch (transportMethod) {
+            case ROAD: return 1.0;
+            case AIR: return 5.0;
+            case SEA: return 2.0;
+            default: throw new IllegalArgumentException("Bilinmeyen taşıma yöntemi!");
+        }
+    }
+
+    // Getter-Setter ve diğer metotlar
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TransportMethod getTransportMethod() {
+        return transportMethod;
+    }
+
+    public void setTransportMethod(TransportMethod transportMethod) {
+        this.transportMethod = transportMethod;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getBaseSpeed() {
+        return baseSpeed;
+    }
+
+    public void setBaseSpeed(double baseSpeed) {
+        this.baseSpeed = baseSpeed;
+    }
+
 }
